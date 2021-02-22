b18cea64e2f61cf729ca893390d6fb111d7ad481
Shyvu <dgshivu3232@gmail.com>
2021-02-22 22:16:38 +0530

    ready for use

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,7 +1,13 @@
-const config = require('config.json');
+const config = require('config');
 const mongoose = require('mongoose');
 const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
-mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions);
+mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions, function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Connected to Mongo successfully!");
+    }
+});
 mongoose.Promise = global.Promise;
 
 module.exports = {
---
+++ config.js
@@ -0,0 +1,14 @@
+﻿const keys = require("./keys.dev");
+
+module.exports = {
+    "connectionString": keys.MONGO_URI,
+    "secret": keys.secret,
+    "emailFrom": "bornanomalouscorp@gmail.com",
+    "smtpOptions": {
+        "service": "gmail",
+        "auth": {
+            "user": "bornanomalouscorp@gmail.com",
+            "pass": keys.GMAIL_PASSWORD
+        }
+    }
+}
